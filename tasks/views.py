9e88783ab82241b93998dcf5fcf4270830574d7a
Sunday Onoja <sundayonoja23@gmail.com>
2025-12-13 21:21:12 +0100

    created CRUD API

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from .models import Task
+from .serializers import TaskSerializer
 
-# Create your views here.
+class TaskViewSet(ModelViewSet):
+    serializer_class = TaskSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Task.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+    @action(detail=True, methods=['post'])
+    def complete(self, request, pk=None):
+        task = self.get_object()
+        task.is_completed = True
+        task.save()
+        return Response({'status': 'Task marked as completed'})
+
+    @action(detail=True, methods=['post'])
+    def incomplete(self, request, pk=None):
+        task = self.get_object()
+        task.is_completed = False
+        task.save()
+        return Response({'status': 'Task marked as incomplete'})
