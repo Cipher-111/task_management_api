38d70e391c29406a6ab5f8092c1b34148d05acc1
Sunday Onoja <sundayonoja23@gmail.com>
2025-12-18 14:01:42 +0100

    feat: implement task CRUD, authentication, and custom actions

--- task/views.py
+++ task/views.py
@@ -1,5 +1,9 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework import status
+
 from .models import Task
 from .serializers import TaskSerializer
 
@@ -13,3 +17,23 @@ class TaskViewSet(ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+
+    @action(detail=True, methods=['post'])
+    def complete(self, request, pk=None):
+        task = self.get_object()
+        task.is_completed = True
+        task.save()
+        return Response(
+            {"message": "Task marked as completed"},
+            status=status.HTTP_200_OK
+        )
+
+    @action(detail=True, methods=['post'])
+    def incomplete(self, request, pk=None):
+        task = self.get_object()
+        task.is_completed = False
+        task.save()
+        return Response(
+            {"message": "Task marked as incomplete"},
+            status=status.HTTP_200_OK
+        )
